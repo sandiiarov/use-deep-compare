4dff41d09f07978f2c3ce119e024ad36e88e5721
Alex Sandiiarov <sandiyarov@gmail.com>
2020-05-30 18:23:36 -0400

    Remove isPrimitive fn

--- src/useDeepCompareMemoize.ts
+++ src/useDeepCompareMemoize.ts
@@ -1,21 +1,12 @@
-import deepEqual from 'dequal';
-import React from 'react';
-
-export function isPrimitive(val: any) {
-  return val == null || /^[sbn]/.test(typeof val);
-}
+import React from "react";
+import deepEqual from "dequal";
 
 export function checkDeps(deps: React.DependencyList, name: string) {
-  const reactHookName = `React.${name.replace(/DeepCompare/, '')}`;
+  const reactHookName = `React.${name.replace(/DeepCompare/, "")}`;
 
   if (!deps || deps.length === 0) {
     throw new Error(
       `${name} should not be used with no dependencies. Use ${reactHookName} instead.`
-    );
-  }
-  if (deps.every(isPrimitive)) {
-    throw new Error(
-      `${name} should not be used with dependencies that are all primitive values. Use ${reactHookName} instead.`
     );
   }
 }
