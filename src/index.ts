46c1ab9fff35866a52100c7c43cb6b6fb00b8ace
sandiiarov <sandiyarov@gmail.com>
2024-07-14 11:16:23 -0400

    update README

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { useDeepCompareCallback } from './useDeepCompareCallback';
 export { useDeepCompareEffect } from './useDeepCompareEffect';
+export { useDeepCompareImperativeHandle } from './useDeepCompareImperativeHandle';
+export { useDeepCompareInsertionEffect } from './useDeepCompareInsertionEffect';
+export { useDeepCompareLayoutEffect } from './useDeepCompareLayoutEffect';
 export { useDeepCompareMemo } from './useDeepCompareMemo';
